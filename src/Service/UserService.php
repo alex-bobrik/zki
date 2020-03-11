65aa22afe9242758e4782550b5babf873c30859b
alex-bobrik <sasha.bobrik@inbox.ru>
2020-03-11 18:39:32 +0300

    Update UserService

--- src/Service/UserService.php
+++ src/Service/UserService.php
@@ -6,23 +6,34 @@ namespace App\Service;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserService
 {
     private $em;
 
-    public function __construct(EntityManagerInterface $em)
+    private $passwordEncoder;
+
+    public function __construct(EntityManagerInterface $em, UserPasswordEncoderInterface $passwordEncoder)
     {
         $this->em = $em;
+        $this->passwordEncoder = $passwordEncoder;
     }
 
     public function createUser(User $user)
     {
         $user->setRoles(['ROLE_USER']);
-        $user->setPassword(null);
+        $user->setPassword('1');
+        $user->setPassword($this->encodePassword($this->passwordEncoder, $user));
+
 
         $this->em->persist($user);
         $this->em->flush();
+    }
+
+    private function encodePassword(UserPasswordEncoderInterface $passwordEncoder, User $user): string
+    {
+        return $passwordEncoder->encodePassword($user, $user->getPassword());
     }
 
     public function updateUser(User $user)
