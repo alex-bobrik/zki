3785f4a3cfcfee2e532d559a9045e42d50cefec2
alex-bobrik <sasha.bobrik@inbox.ru>
2020-03-11 15:12:00 +0300

    Add users output

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,18 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
     /**
-     * @Route("/amdin/", name="admin_users")
+     * @Route("/admin/students", name="admin_students")
      */
     public function index()
     {
+        $users = $this->getDoctrine()->getRepository(User::class)
+            ->findAll();
+
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
+            'users' => $users,
         ]);
     }
 }
