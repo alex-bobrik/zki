671a82d3ff85335f9081071a694ffdaf21bc6a5f
alex-bobrik <sasha.bobrik@inbox.ru>
2020-04-29 18:24:17 +0300

    Add about system page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,53 +16,16 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(StatsService $service)
+    public function index()
     {
         return $this->redirectToRoute('razdel');
+    }
 
-//        $groups = $this->getDoctrine()->getRepository(Groups::class)->findAll();
-//
-//        $resArray = array();
-//
-//
-//        $header = array();
-//        $header[] = 'Группа';
-//        $header[] = 'Ср.балл';
-//
-//        $resArray[] = $header;
-//
-//        foreach ($groups as $item) {
-//            $array = array();
-//            $array[] = $item->getName();
-//            $array[] = $service->getAvgValueInGroupByTests($item);
-//            $resArray[] = $array;
-//        }
-//
-//        $chart = new ColumnChart();
-//
-//        $chart->getData()->setArrayToDataTable(
-//            $resArray
-//        );
-//
-//        $chart->getOptions()->setTitle('Средний балл за тесты по группам');
-//        $chart->getOptions()->setHeight(500);
-//        $chart->getOptions()->setWidth(900);
-//        $chart->getOptions()->getTitleTextStyle()->setBold(true);
-//        $chart->getOptions()->getTitleTextStyle()->setColor('#009900');
-//        $chart->getOptions()->getTitleTextStyle()->setItalic(true);
-//        $chart->getOptions()->getTitleTextStyle()->setFontName('Arial');
-//        $chart->getOptions()->getTitleTextStyle()->setFontSize(20);
-
-//        $group = $this->getDoctrine()->getRepository(Groups::class)->find(1);
-//
-//        $res = $service->getAvgValueByTests($group);
-
-
-
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-            'chart' => $chart,
-            'groups' => $groups,
-        ]);
+    /**
+     * @Route("/system", name="system")
+     */
+    public function aboutSystem()
+    {
+        return $this->render('home/about.html.twig');
     }
 }
