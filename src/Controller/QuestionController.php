c106f3ab53ede3e3e05b021b184b9e5d42598270
alex-bobrik <sasha.bobrik@inbox.ru>
2020-05-12 10:08:21 +0300

    Add questions handling

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,24 +5,26 @@ namespace App\Controller;
 use App\Entity\Question;
 use App\Form\QuestionType;
 use App\Service\TestService;
+use Doctrine\DBAL\Exception\ForeignKeyConstraintViolationException;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class QuestionController extends AbstractController
 {
-//    /**
-//     * @Route("/teacher/questions", name="admin_questions")
-//     */
-//    public function index()
-//    {
-//        $questions = $this->getDoctrine()->getRepository(Question::class)->findAll();
-//
-//        return $this->render('question/index.html.twig', [
-//            'controller_name' => 'QuestionController',
-//            'questions' => $questions,
-//        ]);
-//    }
+    /**
+     * @Route("/teacher/questions", name="admin_questions")
+     */
+    public function index()
+    {
+        $questions = $this->getDoctrine()->getRepository(Question::class)->findAll();
+
+        return $this->render('question/index.html.twig', [
+            'controller_name' => 'QuestionController',
+            'questions' => $questions,
+        ]);
+    }
 
     /**
      * @Route("/teacher/questions/new", name="admin_questions_new")
@@ -41,12 +43,60 @@ class QuestionController extends AbstractController
             $question = $form->getData();
             $testService->saveQuestion($question);
 
-            return $this->redirectToRoute('admin_test_new');
+            return $this->redirectToRoute('admin_questions');
         }
 
         return $this->render('question/new.html.twig', [
             'controller_name' => 'QuestionController',
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/teacher/questions/edit/{id}", name="admin_questions_edit", requirements={"id"="\d+"})
+     * @param Request $request
+     * @param TestService $testService
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function editQuestion(Request $request, TestService $testService, int $id)
+    {
+        $question = $this->getDoctrine()->getRepository(Question::class)->find($id);
+
+        $form = $this->createForm(QuestionType::class, $question);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $question = $form->getData();
+            $testService->saveQuestion($question);
+
+            return $this->redirectToRoute('admin_questions');
+        }
+
+        return $this->render('question/new.html.twig', [
+            'controller_name' => 'QuestionController',
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/teacher/questions/delete/{id}", name="admin_questions_delete", requirements={"id"="\d+"})
+     * @param Request $request
+     * @param TestService $testService
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function deleteQuestion(EntityManagerInterface $em, int $id)
+    {
+        $question = $this->getDoctrine()->getRepository(Question::class)->find($id);
+
+        try {
+            $em->remove($question);
+            $em->flush();
+        } catch (ForeignKeyConstraintViolationException $e) {
+            $this->addFlash('danger', 'Нельзя удалить, из вопроса составлен тест');
+
+            return $this->redirectToRoute('admin_questions');
+        }
+
+        return $this->redirectToRoute('admin_questions');
     }
 }
