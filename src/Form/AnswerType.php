0c2bdd9fd807fdd18addf614dc4d6ba80b668186
alex-bobrik <sasha.bobrik@inbox.ru>
2020-06-11 00:42:52 +0300

    Edit TextType to TextareaType on AnswerType

--- src/Form/AnswerType.php
+++ src/Form/AnswerType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Answer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,7 +15,7 @@ class AnswerType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', TextType::class, [
+            ->add('name', TextareaType::class, [
                 'required' => true,
                 'attr' => ['class' => 'form-control answer-input shadow-none',
                     'maxLength' => '250',
